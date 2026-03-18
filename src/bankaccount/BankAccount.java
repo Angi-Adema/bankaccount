01d67253e58ddf7600247227193bcc545d25932e
Angi-Adema <dr.adema@gmail.com>
2026-03-18 12:35:54 -0600

    added print statements to the accountSummary() method in the BankAccount class.

--- src/bankaccount/BankAccount.java
+++ src/bankaccount/BankAccount.java
@@ -1,5 +1,62 @@
 package bankaccount;
 
 public class BankAccount {
-
+	
+	// Required fields.
+	private String firstName;
+	private String lastName;
+	private int accountID;
+	private double balance;
+	
+	// Constructor that initializes balance to 0.
+	public BankAccount() {
+		balance = 0.0;
+	}
+	
+	// Deposit method accepting single double value.
+	public void deposit(double amount) {
+		balance = balance + amount;
+	}
+	
+	// Withdrawal method accepting single double value.
+	public void withdrawal(double amount) {
+		balance = balance - amount;
+	}
+	
+	// Setters and Getters
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setAccountID(int accountID) {
+		this.accountID = accountID;
+	}
+	
+	public int getAccountID() {
+		return accountID;
+	}
+	
+	// Getter to return balance
+	public double getBalance() {
+		return balance;
+	}
+	
+	// Method to print all account information.
+	public void accountSummary() {
+		System.out.println("Account Summary:");
+		System.out.println("Name: " + firstName + " " + lastName);
+		System.out.printf("Account ID: %d, Balance: $%.2f%n", accountID, balance);
+	}
 }
