edda9d2f53cfe6edc444dedca6e5cd831f6abc32
Angi-Adema <dr.adema@gmail.com>
2026-03-18 14:22:11 -0600

    added interestRate field, no arguments constructor initializing interestRate to 0.0, added the processWithdrawal method to handle a withdrawal and check if account is overdrawn at which time assessing a 30 dollar fee, adding the getter and setter for the interestRate field

--- src/bankaccount/CheckingAccount.java
+++ src/bankaccount/CheckingAccount.java
@@ -13,24 +13,18 @@ public class CheckingAccount extends BankAccount {
 	// Withdrawal method allowing overdraft and assessing $30 charge.
 	public void processWithdrawal(double amount) {
 		
-		// Process withdrawal.
+		// Using superclass BankAccount, process withdrawal.
 		if (amount > 0) {
+			super.withdrawal(amount);
 			
-			// Store new balance by calling getBalance from BankAccount and 
-			// subtracting the withdrawal amount.
-			double newBalance = getBalance() - amount;
-			
-			// Assess fee if balance less than zero after withdrawal.
-			if (newBalance < 0) {
-				newBalance = newBalance - 30.00;
+			// Check new balance and add overdraft fee if account negative.
+			if (getBalance() < 0) {
+				super.withdrawal(30.00);
 				System.out.println("Account overdrawn, $30 overdraft fee assessed.");
 			}
 			
-			// Update the balance in BankAccount.
-			setBalance(newBalance);
-			
 			// Print new current balance.
-			System.out.printf("Current Balance: $%.2f%n", newBalance);
+			System.out.printf("Current Balance: $%.2f%n", getBalance());
 			
 		} else {
 			System.out.println("Please enter an amount greater than zero.");
