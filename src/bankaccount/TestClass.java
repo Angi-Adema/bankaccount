55994cc28dd3f98b7098bd5b325f9fa0cca7bb22
Angi-Adema <dr.adema@gmail.com>
2026-03-18 14:39:54 -0600

    added logic to create a new BankAccount object in TestClass

--- src/bankaccount/TestClass.java
+++ src/bankaccount/TestClass.java
@@ -3,6 +3,21 @@ package bankaccount;
 public class TestClass {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		// Create BankAccount object.
+		BankAccount account = new BankAccount();
+		
+		// Set account data in BankAccount object.
+		account.setFirstName("Jackie");
+		account.setLastName("Wade");
+		account.setAccountID(12345);
+		
+		// Conduct a deposit and withdrawal.
+		account.deposit(500.00);
+		account.withdrawal(150.00);
+		
+		// Print account information from BankAccount class.
+		account.accountSummary();
+		
 	}
 }
